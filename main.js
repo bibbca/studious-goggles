b15338d3d9f1c765776ab8a29b23986238c74505
playernumber21 <bibb.ca@gmail.com>
2017-03-31 23:36:46 -0700

    More Mistakes

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function buyFriend(){
     if(glasses >= friendCost){                                   //checks that the player can afford the friend
         friends = friends + 1;                                   //increases number of friends
     	glasses = glasses - friendCost;                          //removes the glasses spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of friends for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of glasses for the user
+        document.getElementById('friends').innerHTML = friends;  //updates the number of friends for the user
+        document.getElementById('glasses').innerHTML = glasses;  //updates the number of glasses for the user
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,friends));       //works out the cost of the next friend
     document.getElementById('friendCost').innerHTML = nextCost;  //updates the cursor cost for the user
