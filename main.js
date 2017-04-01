34939ea87a525aad4a096884d692bdc3a02d07ef
playernumber21 <bibb.ca@gmail.com>
2017-03-31 23:28:41 -0700

    Copied Example
    http://dhmholley.co.uk/incrementals.html

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var glasses = 0;
-var friends = 0;
-var students = 0;
-var employees = 0;
-var professionals = 0;
+var cookies = 0;
 
-function workPress(number) {
- money = money + number; 
- document.getElementById("money").innerHTML = money;
+function cookieClick(number){
+    cookies = cookies + number;
+    document.getElementById("cookies").innerHTML = cookies;
 };
 
-function buyFriend(){
-    var friendCost = Math.floor(10 * Math.pow(1.1,friends));     //works out the cost of this cursor
-    if(glasses >= friendCost){                                   //checks that the player can afford the cursor
-        friends = friends + 1;                                   //increases number of cursors
-    	glasses = glasses - friendCost;                          //removes the cookies spent
-        document.getElementById('friends').innerHTML = friends;  //updates the number of cursors for the user
-        document.getElementById('glasses').innerHTML = glasses;  //updates the number of cookies for the user
+var cursors = 0;
+
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
+        cursors = cursors + 1;                                   //increases number of cursors
+    	cookies = cookies - cursorCost;                          //removes the cookies spent
+        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
+        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,friends));       //works out the cost of the next cursor
-    document.getElementById('friendCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
-  workPress(friends);
+	
+	cookieClick(cursors);
+	
 }, 1000);
