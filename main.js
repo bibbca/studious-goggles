bd0f6afd1ee1d3ca99cdb5c33cf187aaf07608ae
playernumber21 <bibb.ca@gmail.com>
2017-03-31 23:52:33 -0700

    Added Students / Removed Multiplier

--- main.js
+++ main.js
@@ -19,22 +19,23 @@ function buyFriend(){
     document.getElementById('friendCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
-var multiplier = 1;
+var students = 0;
 
-function buyMultiplier(){
-    var multiplierCost = Math.floor(10 * Math.pow(1.1,multiplier));
-    if(glasses >= multiplierCost){                                   
-        multiplier = multiplier + .1;                                   
-    	glasses = glasses - multiplierCost;                        
-        document.getElementById('multiplier').innerHTML = multiplier;
-        document.getElementById('glasses').innerHTML = glasses;
+function buyStudent(){
+    var friendCost = Math.floor(10 * Math.pow(1.1,students));   
+    if(glasses >= studentCost){                                  
+        students = students + 1;                                
+    	glasses = glasses - studentCost;                        
+        document.getElementById('students').innerHTML = students; 
+        document.getElementById('glasses').innerHTML = glasses;  
     };
-    var nextCost = Math.floor(10 * multiplier));       //works out the cost of the next friend
-    document.getElementById('multiplierCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,students));       //works out the cost of the next friend
+    document.getElementById('friendCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
 	
-	workClick(friends*multiplier);
+	workClick(friends);
+	workClick(students*2);
 	
 }, 1000);
