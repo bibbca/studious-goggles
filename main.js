21185a1b982911d389f1f2e811d8b8c0b9789659
playernumber21 <bibb.ca@gmail.com>
2017-03-31 23:33:51 -0700

    Customized

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var cookies = 0;
+var glasses = 0;
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
+function workClick(number){
+    glasses = glasses + number;
+    document.getElementById("glasses").innerHTML = cookies;
 };
 
 var cursors = 0;
 
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+function buyFriend(){
+    var friendCost = Math.floor(10 * Math.pow(1.1,friends));     //works out the cost of this friend
+    if(glasses >= friendCost){                                   //checks that the player can afford the friend
+        friends = friends + 1;                                   //increases number of friends
+    	glasses = glasses - friendCost;                          //removes the glasses spent
+        document.getElementById('cursors').innerHTML = cursors;  //updates the number of friends for the user
+        document.getElementById('cookies').innerHTML = cookies;  //updates the number of glasses for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,friends));       //works out the cost of the next friend
+    document.getElementById('friendCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
 	
-	cookieClick(cursors);
+	workClick(friend);
 	
 }, 1000);
